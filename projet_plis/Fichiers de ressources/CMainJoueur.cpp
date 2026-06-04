b194bd0729c2d28c79d96c4ceb56eadb581375a4
Mathischtn <mathis.chatonnet@gmail.com>
2026-06-04 09:36:24 +0200

    c

---
+++ projet_plis/Fichiers de ressources/CMainJoueur.cpp
@@ -0,0 +1,20 @@
+#include "CMainJoueur.h"
+
+void CMainJoueur::retirer(CCarte* carte)
+{
+	int cpt = 0;
+	while (vCartes[cpt] != cartes && vCartes.size() <= cpt) {
+		cpt += 1;
+	}
+	if (vCartes[cpt] == cartes) {
+		vCartes.erase(vCartes.begin() + cpt);	
+	}
+	else {
+		cout << "Vous ne pouvez pas retirer cette carte" << endl; //class CEXCEPTION
+	}
+}
+
+vector<CCarte*> CMainJoueur::getCartes()
+{
+	return vector<CCarte*>();
+}
