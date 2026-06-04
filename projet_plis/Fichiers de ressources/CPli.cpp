c26b4fdd166aadfbf58529eb7898747a06c44467
lucas <lucas.rouget@etu.univ-tours.fr>
2026-06-04 09:35:55 +0200

    modif CJoueur , CPli et CPLI.cpp

--- projet_plis/Fichiers de ressources/CPli.cpp
+++ projet_plis/Fichiers de ressources/CPli.cpp
@@ -2,5 +2,16 @@
 
 void CPli::ajouterCartes(CJoueur& Joueur, CEnsembleCartes& Cartes)
 {
-
+	paire_joueur_cartes.push_back(Joueur, Cartes);
 }
+
+void CPli::vider()
+{
+	paire_joueur_cartes.clear();
+}
+
+void CPli::DonnerCartesVainqueur(CJoueur & Joueur)
+{
+	Joueur.
+}
+
